0f7357c9a794a405c1e82c8a0b52e0b310c03cc9
Manu Dawber <manu.dawber@enterpriseit.co.nz>
2019-06-06 21:25:44 +1200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM loadimpact/k6
+
+RUN mkdir /k6
+COPY ./* /k6/
+
---
+++ script.js
@@ -0,0 +1,29 @@
+import { check, sleep } from "k6"
+import encoding from "k6/encoding";
+import http from "k6/http";
+
+const url = `${__ENV.ELASTIC_URL}`,
+      index = `${__ENV.ELASTIC_INDEX}`,
+      user = `${__ENV.ELASTIC_USER}`,
+      passwd = `${__ENV.ELASTIC_PASSWD}`,
+      payload = open("/k6/config/query.json");
+
+export let options = {
+    duration: `${__ENV.TEST_DURATION}`,
+    vus: `${__ENV.TEST_NUM_USERS}`
+};
+
+export default function() {
+    let endpoint = `${url}/${index}/_search`
+    let params = {
+        headers: {
+            "Authorization": "Basic " + encoding.b64encode(`${user}:${passwd}`),
+            "Content-Type": "application/json"
+        }
+    };
+    let res = http.post(endpoint, payload, params);
+    check(res, {
+        "is status 200": (r) => r.status === 200
+    });
+    sleep(1);
+};
